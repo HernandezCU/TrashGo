0536223ffd1cdf009c02b3ac3a16751116b0cc8c
hernandezcu <hernandezcu@jbu.edu>
2023-04-01 16:28:58 -0500

    sss

--- TrashGo/MapViewController.swift
+++ TrashGo/MapViewController.swift
@@ -8,9 +8,18 @@
 import UIKit
 import MapKit
 import CoreLocation
+import Alamofire
 
 class MapViewController: UIViewController, MKMapViewDelegate {
     
+    struct reply: Codable{
+        let success: Int
+        let error: Bool
+    }
+    
+    var mode = ""
+    
+    @IBOutlet weak var btn: UIButton!
     @IBOutlet weak var map: MKMapView!
     
     @IBOutlet weak var name: UILabel!
@@ -23,6 +32,7 @@ class MapViewController: UIViewController, MKMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        check()
         map.setRegion(MKCoordinateRegion(center: coord, span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)), animated: true)
         map.delegate = self
         DispatchQueue.main.async{
@@ -32,16 +42,46 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         }
         
         addPin()
-
-
     }
     
+    func check(){
+        
+        let parameters: Parameters = ["email": "matt@matt.com", "address": gdata[gindex].address]
+        
+        let request = AF.request("http://127.0.0.1:5000/check", method: .post, parameters: parameters, encoding: JSONEncoding.default)
+                                                      
+        request.responseDecodable(of: reply.self) { (response) in
+            print("here4")
+            guard let response = response.value else {
+                print("Failed")
+                print(response)
+                return }
+            print("Success")
+            // self.spinner.stopAnimating()
+            
+                DispatchQueue.main.async {
+                    if response.error == true {
+                        self.btn.isEnabled = true
+                        self.btn.configuration?.title = "Claim Raid!"
+                        self.mode = "raid"
+                    }
+                    else{
+                        self.btn.isEnabled = true
+                        self.mode = "away"
+                    
+                    }
+                }
+                
+            }
+            
+            
+    }
     
     private func addPin(){
         let pin  =  MKPointAnnotation()
         pin.coordinate = coord
-        pin.title = "Park"
-        pin.subtitle = "Clean it"
+        pin.title = gdata[gindex].title
+        pin.subtitle = "A great place so lets take care of it."
         map.addAnnotation(pin)
     }
     
@@ -58,6 +98,53 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         }
         annotationView?.image = UIImage(systemName: "pin.fill")?.withTintColor(.red)
         return annotationView
+    }
+    
+    
+    @IBAction func click(_ sender: Any) {
+        
+        if mode == "raid"{
+            let parameters: Parameters = ["email": "matt@matt.com"]
+            
+            let request = AF.request("http://127.0.0.1:5000/finish_raid", method: .post, parameters: parameters, encoding: JSONEncoding.default)
+                                                          
+            request.responseDecodable(of: reply.self) { (response) in
+                print("here4")
+                guard let response = response.value else {
+                    print("Failed")
+                    print(response)
+                    return }
+                print("Success")
+                // self.spinner.stopAnimating()
+                    
+                }
+        }
+        else{
+            
+            var locManager = CLLocationManager()
+            locManager.requestWhenInUseAuthorization()
+            
+            var currentLocation: CLLocation!
+
+            if
+               CLLocationManager.authorizationStatus() == .authorizedWhenInUse ||
+               CLLocationManager.authorizationStatus() ==  .authorizedAlways
+            {
+                currentLocation = locManager.location
+            }
+            
+            
+            let source = MKMapItem(placemark: MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: currentLocation.coordinate.longitude, longitude: currentLocation.coordinate.latitude)))
+            source.name = "Source"
+                    
+            let destination = MKMapItem(placemark: MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: CLLocationDegrees(gdata[gindex].lat), longitude: CLLocationDegrees(gdata[gindex].lng))))
+            destination.name = "Destination"
+                    
+            MKMapItem.openMaps(
+              with: [source, destination],
+              launchOptions: [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
+            )
+        }
     }
     /*
     // MARK: - Navigation
